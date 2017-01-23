c76b0b31d0712d4deee75b03d3e94fc8dab305ca
gozu66 <gozu66@gmail.com>
2017-01-23 21:05:15 +0000

    Enemy Movement started

---
+++ USI_STI Invaders/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+	void Start ()
+    {
+        InvokeRepeating("Tick", 1f, 1f);
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    void Tick()
+    {
+        //Enemy moves left or right
+        //Sound is played
+        //If enemy block has reached screen edge, mocve down, revese direction
+        transform.Translate(new Vector3(1, 0, 0));
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("hghhgh");
+        if (other.tag == "MainCamera")
+        {
+            Debug.Log("hghhgh");
+        }
+    }
+}
