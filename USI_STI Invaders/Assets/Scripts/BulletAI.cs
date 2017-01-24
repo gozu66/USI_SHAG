45768bc919df0282134e4a88d920d8235edd5d5c
RichieDevEire <c15707085@mydit.ie>
2017-01-24 18:31:06 +0000

    Finally my commit

---
+++ USI_STI Invaders/Assets/Scripts/BulletAI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletAI : MonoBehaviour 
+{
+	public float speed;
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 newPos = transform.position;
+		newPos.y += speed * Time.deltaTime;
+		transform.position = newPos;
+	}
+}
---
+++ USI_STI Invaders/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour 
+{
+	public float playerSpeed;
+	//public float smoothTime = 0.4f;
+	public float minX = -5.0f;
+	public float maxX = 5.0f;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		float myDir = Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime;
+
+		transform.position += new Vector3(myDir, 0,0f);
+
+		if(transform.position.x < minX)
+		{
+			transform.position = new Vector3(minX, transform.position.y, transform.position.z);
+		}
+		else if(transform.position.x > maxX)
+		{
+			transform.position = new Vector3(maxX, transform.position.y, transform.position.z);
+		}
+	}
+}
