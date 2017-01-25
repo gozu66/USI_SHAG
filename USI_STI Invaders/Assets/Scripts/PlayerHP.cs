2ea98641a599b83cc5f8abfa83da26077dac1328
RichieDevEire <c15707085@mydit.ie>
2017-01-25 01:34:49 +0000

    Player health with UI and damage basic set up

---
+++ USI_STI Invaders/Assets/Scripts/PlayerHP.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHP : MonoBehaviour 
+{
+	public float playerHP;
+	public Text healthText;
+
+	public int basicDamage = 10;
+	// Use this for initialization
+	void Start () 
+	{
+		playerHP = 100;
+		SetHealthText();
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+
+		if(other.gameObject.tag == "Respawn")
+		{
+			// use this for pooling when ready
+			//other.gameObject.SetActive(false);
+			playerHP -= basicDamage;
+			SetHealthText();
+		}
+	}
+	void SetHealthText()
+	{
+		healthText.text = "Health: " + playerHP.ToString ();
+	
+	}
+}
