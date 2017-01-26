f93daa5193fbfd898229655e304e145732655b1b
gozu66 <gozu66@gmail.com>
2017-01-26 22:35:09 +0000

    Enemy group movement logic implimented

--- USI_STI Invaders/Assets/Scripts/EnemyAI.cs
+++ USI_STI Invaders/Assets/Scripts/EnemyAI.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+    public enum EnemyType
+    {
+        Chlamydia,
+        Gonorrhoea,
+        Herpes,
+        GenitalWarts,
+        HIV
+    };
+    public EnemyType enemyType;
+
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Respawn")
@@ -17,6 +28,28 @@ public class EnemyAI : MonoBehaviour
     public GameObject bullet;
     public void Shoot()
     {
-        Instantiate(bullet, transform.position, Quaternion.identity);
+        GameObject aBullet;
+        aBullet = Instantiate(bullet, transform.position, Quaternion.identity) as GameObject;
+
+        switch (enemyType)
+        {
+            case EnemyType.Chlamydia:
+                aBullet.GetComponent<EnemyBullet>().speed = 1;
+                break;
+
+            case EnemyType.Gonorrhoea:
+                aBullet.GetComponent<EnemyBullet>().speed = 5;
+                break;
+
+            case EnemyType.Herpes:
+                break;
+
+            case EnemyType.GenitalWarts:
+                break;
+
+            case EnemyType.HIV:
+                break;
+        }
+        
     }
 }
